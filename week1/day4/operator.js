44e27c2185fa6b975585aaeb623cb1e1a094b3bc
Aldin Wildan <08.aldinwildan@gmail.com>
2024-03-28 19:04:03 +0700

    belajar dasar Javascript

---
+++ week1/day4/operator.js
@@ -0,0 +1,29 @@
+var penambahan = (6+9)  //15
+var pengurangan = (9-6) //3
+var perkalian = (9*6)   //54
+var pembagian = (9/3)   //3
+var hasilbagi = (3%2)   //1
+
+//equal operator (==)
+var angka = 100
+console.log(angka == 100) // true
+console.log(angka == 20) // false
+
+// not equal
+var sifat = "rajin"
+console.log(sifat != "malas") // true
+console.log(sifat != "bandel") //true 
+
+// strict equal (membandingkan value dan tipe data)
+var angka = 8
+console.log(angka == "8") // true, padahal "8" adalah string.
+console.log(angka === "8") // false, karena tipe data nya berbeda
+console.log(angka === 8) // true 
+
+
+
+console.log(penambahan)
+console.log(pengurangan)
+console.log(perkalian)
+console.log(pembagian)
+console.log(hasilbagi)
---
+++ week1/day4/var.js
@@ -0,0 +1,13 @@
+// kalo php kan echo (nama fungsi) = deklarasi
+// js juga sama var (nama fungsi) = deklarasi
+
+var hello = "Hello World!"
+console.log(hello) // Output: Hello World!
+
+var name = "john"
+var age = 20
+var todayIsFriday = false
+
+console.log(name)
+console.log(age)
+console.log(todayIsFriday)
